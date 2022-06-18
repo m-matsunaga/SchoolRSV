9ac822ee1720577d9678615385eda727c8cd71c0
松永美緒理 <>
2022-06-18 12:09:41 +0900

    提出1

--- config/app.php
+++ config/app.php
@@ -229,8 +229,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'authorityClass' => app\Common\authorityClass::class
-
+        'authorityClass' => app\Common\authorityClass::class,
+        'FrameRegister' => app\FrameRegister\FrameRegister::class
     ],
 
 ];
