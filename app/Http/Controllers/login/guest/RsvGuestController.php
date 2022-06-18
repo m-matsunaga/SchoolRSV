7ddeab09c447d02759b2299cf3ed8d342fbdda23
松永美緒理 <>
2022-06-18 11:25:49 +0900

    提出1

--- app/Http/Controllers/login/guest/RsvGuestController.php
+++ app/Http/Controllers/login/guest/RsvGuestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\login\guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RsvGuestController extends Controller
 {
@@ -13,6 +14,7 @@ class RsvGuestController extends Controller
     $deleteRsvData = \App\Reserve::where('id', $id)
                     ->first();
 
+    DB::transaction(function () use($deleteRsvData, $id) {
     \App\RsvFrameCount::where('date', $deleteRsvData->rsv_date)
                         ->where('rsv_frame_id', $deleteRsvData->rsv_frame_id)
                         ->increment('frame_count');
@@ -20,6 +22,7 @@ class RsvGuestController extends Controller
     \App\Reserve::
         where('id', $id)
         ->delete();
+    });
 
         return redirect('/guest/home');
 }
@@ -33,19 +36,19 @@ class RsvGuestController extends Controller
 
         if ($value) {
 
-            \App\Reserve::insert([
-                'user_id' => $id,
-                'rsv_date' => $date_key,
-                'rsv_frame_id' => $value,
-            ]);
+            DB::transaction(function () use($id, $date_key, $value) {
+                \App\Reserve::insert([
+                    'user_id' => $id,
+                    'rsv_date' => $date_key,
+                    'rsv_frame_id' => $value,
+                ]);
 
-            \App\RsvFrameCount::where('date', $date_key)
-                                ->where('rsv_frame_id', $value)
-                                ->decrement('frame_count');
+                \App\RsvFrameCount::where('date', $date_key)
+                                    ->where('rsv_frame_id', $value)
+                                    ->decrement('frame_count');
+            });
         }
     }
-
         return redirect('/guest/home');
-   }
-
+    }
 }
